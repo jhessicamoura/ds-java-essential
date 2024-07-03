fe21043073a3e66f74fe9f93873404cc5bed03a0
Jhéssica Moura <jhessica.moura7@gmail.com>
2024-07-03 17:03:19 -0300

    Pagamento

---
+++ estruturasequencial/Pagamento.java
@@ -0,0 +1,28 @@
+package estruturasequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Pagamento {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        double valorHora, pagamento;
+        int horasTrabalhadas;
+
+        System.out.print("Nome: ");
+        nome = sc.nextLine();
+        System.out.print("Valor por hora: ");
+        valorHora = sc.nextDouble();
+        System.out.print("Horas trabalhadas: ");
+        horasTrabalhadas = sc.nextInt();
+
+        pagamento = valorHora * horasTrabalhadas;
+        System.out.printf("O pagamento para %s deve ser %.2f", nome, pagamento);
+
+    }
+}
