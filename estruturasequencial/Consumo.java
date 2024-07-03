8c3a8fbf279a4fca7c0630cd21557976548fe816
Jhéssica Moura <jhessica.moura7@gmail.com>
2024-07-03 17:18:56 -0300

    Consumo

---
+++ estruturasequencial/Consumo.java
@@ -0,0 +1,25 @@
+package estruturasequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Consumo {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int distancia;
+        double mediaConsumo, combustivelGasto;
+
+        System.out.print("Distancia percorrida: ");
+        distancia = sc.nextInt();
+        System.out.print("Combustível gasto: ");
+        combustivelGasto = sc.nextDouble();
+
+        mediaConsumo = distancia / combustivelGasto;
+        System.out.printf("Combustível medio = %.3f", mediaConsumo);
+
+    }
+}
